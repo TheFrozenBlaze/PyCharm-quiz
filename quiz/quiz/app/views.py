05e7b7750115f1a650b8f9dffa9a7ab73073e8b5
FrozenBlaze - Radon Team <engine00er@gmail.com>
2026-02-23 15:06:15 +0100

    Add files via upload

--- quiz/quiz/app/views.py
+++ quiz/quiz/app/views.py
@@ -9,8 +9,22 @@ from .models import Question
 def view_question(request, question_id):
     question = Question.objects.get(id=question_id)
 
+    if request.method == "POST":
+        if question.answer == request.POST['Answer']:
+            feedback = "Helyes válasz"
+        else:
+            feedback = "Helytelen válasz"
+        selected = request.POST['Answer']
+
+    else:
+        feedback = None
+        selected = None
+
+
     context = {
-        'question': question
+        'question': question,
+        'feedback': feedback,
+        'selected': selected
     }
 
     return render(request, 'app/hello.html', context)
